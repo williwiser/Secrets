ba5b981305051dfe18f01e554243594cd8682508
William <williwiser@gmail.com>
2024-05-14 18:12:41 +0200

    modify env

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
-import env from "dotenv";
-import pg from "pg";
+import "dotenv/config";
 const { Pool } = pg;
 
 const app = express();
 const port = process.env.PORT;
 const saltRounds = 10;
-env.config();
 
 app.use(
   session({
